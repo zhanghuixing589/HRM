6f42ad19baedf1e0dba454b467ad14108076b263
张惠兴 <13531088173@163.com>
2025-11-26 10:08:56 +0800

    创建项目框架

---
+++ src/main/java/org/example/hrm/HrmApplication.java
@@ -0,0 +1,13 @@
+package org.example.hrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrmApplication.class, args);
+    }
+
+}
